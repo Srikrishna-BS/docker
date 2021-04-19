02eab3bb455c87f3c0759e266d42f88efa9519b4
krish <krishnabsirl@gmail.com>
2021-04-19 11:47:50 +0100

    working on redis compose

---
+++ nodejs/docker-compose-redis/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+    host: 'redis',
+    port: 6379
+});
+client.set('visists',0);
+
+app.get('/', (req, res)=>{
+    client.get('visits',(err,visits)=>{
+        res.send("number of visits"+visits);
+        client.set('visits',parseInt(visits)+1);
+    });
+});
+
+app.listen(8081, ()=>{
+    console.log("Listening on this lawda port 8081");
+});
